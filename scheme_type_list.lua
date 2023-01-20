7e5deb2006e1eed4f57ea17f7b9d5595c089b565
Brad Odenath <brad.odenath@gmail.com>
2023-01-20 00:36:39 -0500

    lol crashing

--- scheme_type_list.lua
+++ scheme_type_list.lua
@@ -1,50 +1,70 @@
+alphabet = {
+	range = {
+		low = 97,
+		high = 122
+	},
+	letters = function()
+		ltrs = {}
+		for i = alphabet.range.low, alphabet.range.high do
+			table.insert(ltrs,string.char(i))
+		end
+		return ltrs
+	end
+}
+
 url = {
-	schemes = {"http","https"},
+	_schemes = {"http","https"},
 	
-	colonslashslash = "://",
+	slash = '/',
 	dot = '.',
 	
-	tld = {
+	_tld = {
 		'com',
 		'net',
 		'org',
 		'me',
 		'io',
-	}
+	},
+	
+	schemes = function()
+		__schemes = {}
+		for i,v in pairs(url._schemes) do
+			table.insert(__schemes,(v..(url.colonslashslash)))
+		end
+		return (__schemes)
+	end,
+	
+	tld = function()
+		__tld = {}
+		for i,v in pairs(url._tld) do
+			table.insert(__tld,url.dot..(v))
+		end
+		return (__tld)
+	end
 	
 }
+url.colonslashslash = (":"..url.slash..url.slash)
+url.print_all_possible_urls = function(ayo)
+	for i,v in pairs(url.schemes()) do		
+		for j,w in pairs(alphabet.letters()) do
+			for k,x in pairs(url.tld()) do
+				_ayo = (((ayo)~=nil) and (ayo) or '')
+				_url = (v..w.._ayo..x)
+				url.print_all_possible_urls(_ayo..w)
+				print(_url)
+end	end end end
+url.print_all_possible_urls()
 
-alphabet = {
-	range = {
-		low = 97,
-		high = 122
-	}
-}
-
-url.protocols = function()
-	schemes = {}
-	for i,v in pairs(url.schemes) do
-		table.insert(schemes,(v..url.colonslashslash))
-	end
-	return (schemes)
-end
-
-alphabet.letters = function()
-	ltrs = {}
-	for i = alphabet.range.low, alphabet.range.high do
-		table.insert(ltrs,string.char(i))
-	end
-	return ltrs
-end
 
 print_table = function(tbl)
 	comma = false
 	out = ('{ ')
 	for i,v in pairs(tbl) do
-		out = (comma and (out..', '..v) or (out..v))
+		out = (comma and (out..', '..v) or (out..(v)))
 		if (not comma) then comma = true end
 	end
 	out = (out..' }')
 	print(out)
 end
+
 print_table(alphabet.letters())
